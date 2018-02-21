e98d4a665824f94961b0215e0e457097d0c1a6a0
Pavel Mayboroda <Successbloger1@gmail.com>
2018-02-21 17:15:11 +0200

    Small fix

--- Matchismo/ViewController.m
+++ Matchismo/ViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "ViewController.h"
-#import "PMDeck.h"
 #import "PMPlayingCardDeck.h"
 #import "PMPlayingCard.h"
 
@@ -16,7 +15,6 @@
 
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (assign, nonatomic) NSInteger flipsCount;
-
 
 @property (strong, nonatomic) PMDeck *deck;
 
@@ -29,18 +27,21 @@
 - (IBAction)touchCardAction:(UIButton *)sender {
 
     if ([sender.currentTitle length]) {
+        
         [sender setBackgroundImage: [UIImage imageNamed: @"cardBack"] forState: UIControlStateNormal];
         [sender setTitle: @"" forState: UIControlStateNormal];
+        
     } else {
         
-        PMPlayingCard *card = (PMPlayingCard *)[self.deck drawRandomCard];
+        PMCard *card = [self.deck drawRandomCard];
         
         if (card) {
             [sender setBackgroundImage: [UIImage imageNamed: @"cardFront"] forState: UIControlStateNormal];
             [sender setTitle: [card contents] forState: UIControlStateNormal];
+            
+            self.flipsCount++;
         }
     }
-    self.flipsCount++;
 }
 
 #pragma mark - Setters&Getters
@@ -54,10 +55,17 @@
 - (PMDeck *) deck {
     
     if (!_deck) {
-        _deck = [[PMPlayingCardDeck alloc] init];
+        _deck = [self createDeck];
     }
     
     return _deck;
+}
+
+#pragma mark - Help Methods
+
+- (PMDeck *) createDeck {
+    
+    return [[PMPlayingCardDeck alloc] init];
 }
 
 
